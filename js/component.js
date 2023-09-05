df0c30297af34398e3d51fe03b1dccb82b88700d
kairuec <sakurai1991@gmail.com>
2023-09-05 17:50:44 +0900

    first

--- js/component.js
+++ js/component.js
@@ -1,16 +1,16 @@
 const Component = class {
 
-    constructor(url) {
-        this.url = url;
+    constructor(state) {
+        this.state = state;
     }
 
     //リンク先のサイト
     site() {
-        return 'https://www.by-the-sea.info/SHOP/' + this.url + '.html';
+        return 'https://www.by-the-sea.info/SHOP/' + this.state.url + '.html';
     }
 
     //現在のリングの画像と値段
-    itemComponent(state) {
+    itemComponent() {
         return (`
             <h2 class="kanseiImage">完成イメージ</h2>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -18,41 +18,127 @@ const Component = class {
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" />
             </svg>
-            <img src="https://www.by-the-sea.info/images/item/` + state.url + `.jpg" height="250px" class="sp_only">
-            <img src="https://www.by-the-sea.info/images/item/` + state.url + `.jpg" width="100%" class="pc_only">
-            <p class="sp_price"><span>` + Number(state.price).toLocaleString() + `</span>円(税込)</p>
+            <img src="https://www.by-the-sea.info/images/item/` + this.state.url + `.jpg" height="250px" class="sp_only">
+            <img src="https://www.by-the-sea.info/images/item/` + this.state.url + `.jpg" width="100%" class="pc_only">
+            <p class="sp_price"><span>` + Number(this.state.price).toLocaleString() + `</span>円(税込)</p>
+            `);
+    }
+
+    //選択中
+    selected() {
+
+        return (`
+            <ul class="status">
+                <li>地金：` + this.state.material + `</li>
+                <li>幅：` + this.state.width + `</li>
+                <li>厚み：` + this.state.thick + `</li>
+                <li>彫り：` + this.state.cave + `</li>
+                <li>エッジ：` + this.state.edge + `</li>
+                <li>形状：` + this.state.type + `</li>
+            </ul>
+            <p class="price"><span>` + Number(this.state.price).toLocaleString() + `</span>円（税込）</p>
             `);
     }
 
     //現在の選択中のリングの情報
-    statusComponent(state) {
+    statusComponent() {
         return (`
             <ul class="status">
-                <li>地金：` + state.material + `</li>
-                <li>幅：` + state.width + `</li>
-                <li>厚み：` + state.thick + `</li>
-                <li>彫り：` + state.cave + `</li>
-                <li>エッジ：` + state.edge + `</li>
-                <li>形状：` + state.type + `</li>
+                <li>地金：` + this.state.material + `</li>
+                <li>幅：` + this.state.width + `</li>
+                <li>厚み：` + this.state.thick + `</li>
+                <li>彫り：` + this.state.cave + `</li>
+                <li>エッジ：` + this.state.edge + `</li>
+                <li>形状：` + this.state.type + `</li>
             </ul>
-            <p class="price"><span>` + Number(state.price).toLocaleString() + `</span>円（税込）</p>
+            <p class="price"><span>` + Number(this.state.price).toLocaleString() + `</span>円（税込）</p>
             `);
     }
 
     //選択ボタンをまとめる
-    joinComponent(array) {
+    joinComponent(array, string) {
+
+        const nextStep = (next) => {
+            return (`
+                    <div align="center" onclick="tab('`+ next + `')" >
+                            <div class="next_step">次のSTEPへ
+                                <span class="u-icon--arrow-right"></span>
+                            </div>
+                    </div>
+                    `);
+        }
+
+        const tubContent = (prop) => {
+            switch (prop) {
+                case 'material':
+                    return {
+                        step: 'STEP1',
+                        title: '地金を選ぶ',
+                        content: nextStep('.widthComponent')
+                    }
+                case 'width':
+                    return {
+                        step: 'STEP2',
+                        title: '幅を選ぶ',
+                        content: nextStep('.thickComponent')
+                    }
+                case 'thick':
+                    return {
+                        step: 'STEP3',
+                        title: '厚みを選ぶ',
+                        content: nextStep('.caveComponent')
+                    }
+                case 'cave':
+                    return {
+                        step: 'STEP4',
+                        title: '彫りを選ぶ',
+                        content: nextStep('.edgeComponent')
+                    }
+                case 'edge':
+                    return {
+                        step: 'STEP5',
+                        title: 'エッジ（縁）を選ぶ',
+                        content: nextStep('.typeComponent')
+                    }
+                case 'type':
+                    return {
+                        step: 'STEP6',
+                        title: '形状を選ぶ',
+                        content:
+                            `
+                            <h3>バレルとフラットのリング断面図</h3>
+                            <div class="margin_bottom_10"></div>
+                            <p class="ex">バレルはフラットから角をとっているため柔らかな印象ですが、同じ厚みでもフラットの方より厚い印象に仕上がります。</p>
+                            <table border="0" cellspacing="10" style="text-align:center;">
+                                <tbody>
+                                    <tr>
+                                        <td>バレルの断面図<img src="https://www.by-the-sea.info/images/ordermade/barrel_.webp"
+                                                width="100%"></td>
+                                        <td>フラットの断面図<img src="https://www.by-the-sea.info/images/ordermade/flat_.webp"
+                                                width="100%">
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            `,
+                    }
+            }
+        }
         return (`
         <div class="selects">
-            <h2><span class="step">STEP1</span>地金を選ぶ</h2>
+            <h2><span class="step">` + tubContent(string).step + `</span>` + tubContent(string).title + `</h2>
             <ul class="horizontal_scroll">` + array.join('') + `</ul>
+            ` + tubContent(string).content + `
         </div>
         `);
+
     }
 
+
     //選択するボタン
-    buttonComponent(content, difference) {
+    buttonComponent(url, content, difference) {
         return (`
-                <li onclick="change('`+ this.url + `')">
+                <li onclick="change('`+ url + `')" ` + this.buttonSelected(url) + `>
                     <img src="https://www.by-the-sea.info/images/ordermade/`+ this.buttonImage(content) + `.webp">
                     <p>`+ content + `</p>
                     <span>`+ difference + `</span>
@@ -61,14 +147,24 @@ const Component = class {
     }
 
     //選択できない時用のボタン
-    disabledComponent(content, error) {
+    disabledComponent(url, content, error) {
         return (`
-            <li>
+            <li  ` + this.buttonSelected(url) + `>
                 <img src="https://www.by-the-sea.info/images/ordermade/`+ this.buttonImage(content) + `.webp">
                 <p>`+ content + `</p>
                 <span style="color: #FF0000;">`+ error + `</span>
             </li>
         `);
+    }
+
+    //選択中のボタン
+    buttonSelected(url) {
+        if (url == this.state.url) {
+            return 'class="selected"'
+        }
+        else {
+            return '';
+        }
     }
 
     //ボタン画像の切替
--- js/ordermade.js
+++ js/ordermade.js
@@ -41,20 +41,22 @@ function formatJSON(json, itemUrl) {
     const materialComponent = (meterials) => {
         const materialList = meterials.map((material) => {
             if (material.material == 'シルバー925' && material.thick == '厚み1.2mm') {
-                return new Component(material.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    material.url,
                     material.material,
                     '厚み1.2mmを選択中のため不可'
                 );
             }
             else {
-                return new Component(material.url).buttonComponent(
+                return new Component(state).buttonComponent(
+                    material.url,
                     material.material,
                     differencePrice(material.price)
                 );
             }
         });
         //ボタンをまとめる
-        return new Component().joinComponent(materialList);
+        return new Component().joinComponent(materialList, 'material');
     }
 
     //形状に関する処理
@@ -65,13 +67,14 @@ function formatJSON(json, itemUrl) {
     //バリデーション＆コンポーネント呼び出し
     const typeComponent = (types) => {
         const typeList = types.map((type) => {
-            return new Component(type.url).buttonComponent(
+            return new Component(state).buttonComponent(
+                type.url,
                 type.type,
                 differencePrice(type.price)
             );
         });
         //ボタンをまとめる
-        return new Component().joinComponent(typeList);
+        return new Component().joinComponent(typeList, 'type');
     }
 
     //幅に関する処理
@@ -84,38 +87,43 @@ function formatJSON(json, itemUrl) {
         const widthList = widths.map((width) => {
             if (width.material == 'シルバー925' && width.thick == '厚み1.2mm') {
 
-                return new Component(width.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    width.url,
                     width.width,
                     'シルバー925を選択中のため不可'
                 );
             }
             else if (width.width == '幅4mm' && width.edge == 'プレーン') {
-                return new Component(width.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    width.url,
                     width.width,
                     'エッジ「プレーン」を選択中のため不可'
                 );
             }
             else if (width.width == '幅4mm' && width.edge == 'ダイヤモンドカット') {
-                return new Component(width.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    width.url,
                     width.width,
                     'エッジ「ダイヤモンドカット」を選択中のため不可'
                 );
             }
             else if (width.width == '幅4mm' && width.edge == 'コインエッジ') {
-                return new Component(width.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    width.url,
                     width.width,
                     'エッジ「コインエッジ」を選択中のため不可'
                 );
             }
             else {
-                return new Component(width.url).buttonComponent(
+                return new Component(state).buttonComponent(
+                    width.url,
                     width.width,
                     differencePrice(width.price)
                 );
             }
         });
         //ボタンをまとめる
-        return new Component().joinComponent(widthList);
+        return new Component().joinComponent(widthList, 'width');
     }
 
 
@@ -128,20 +136,22 @@ function formatJSON(json, itemUrl) {
     const thickComponent = (thicks) => {
         const thickList = thicks.map((thick) => {
             if (thick.material == 'シルバー925' && thick.thick == '厚み1.2mm') {
-                return new Component(thick.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    thick.url,
                     thick.thick,
                     'シルバー925を選択中のため不可'
                 );
             }
             else {
-                return new Component(thick.url).buttonComponent(
+                return new Component(state).buttonComponent(
+                    thick.url,
                     thick.thick,
                     differencePrice(thick.price)
                 );
             }
         });
         //ボタンをまとめる
-        return new Component().joinComponent(thickList);
+        return new Component().joinComponent(thickList, 'thick');
     }
 
     //彫りに関する処理
@@ -152,13 +162,14 @@ function formatJSON(json, itemUrl) {
     //バリデーション＆コンポーネント呼び出し
     const caveComponent = (caves) => {
         const caveList = caves.map((cave) => {
-            return new Component(cave.url).buttonComponent(
+            return new Component(state).buttonComponent(
+                cave.url,
                 cave.cave,
                 differencePrice(cave.price)
             );
         });
         //ボタンをまとめる
-        return new Component().joinComponent(caveList);
+        return new Component().joinComponent(caveList, 'cave');
     }
 
     //縁に関する処理
@@ -170,38 +181,42 @@ function formatJSON(json, itemUrl) {
     const edgeComponent = (edges) => {
         const edgeList = edges.map((edge) => {
             if (edge.width == '幅4mm' && edge.edge == 'プレーン') {
-                return new Component(edge.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    edge.url,
                     edge.edge,
                     '幅4mmを選択中のため不可'
                 );
             }
             else if (edge.width == '幅4mm' && edge.edge == 'ダイヤモンドカット') {
-                return new Component(edge.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    edge.url,
                     edge.edge,
                     '幅4mmを選択中のため不可'
                 );
             }
             else if (edge.width == '幅4mm' && edge.edge == 'コインエッジ') {
-                return new Component(edge.url).disabledComponent(
+                return new Component(state).disabledComponent(
+                    edge.url,
                     edge.edge,
                     '幅4mmを選択中のため不可'
                 );
             }
             else {
-                return new Component(edge.url).buttonComponent(
+                return new Component(state).buttonComponent(
+                    edge.url,
                     edge.edge,
                     differencePrice(edge.price)
                 );
             }
         });
         //ボタンをまとめる
-        return new Component().joinComponent(edgeList);
+        return new Component().joinComponent(edgeList, 'edge');
     }
 
     //現在の商品のステータスをページ上に反映
-    document.querySelector(".itemComponent").innerHTML = new Component().itemComponent(state);
-    document.querySelector(".stateComponent").innerHTML = new Component().statusComponent(state);
-    document.querySelector(".sp_stateComponent").innerHTML = new Component().statusComponent(state);
+    document.querySelector(".itemComponent").innerHTML = new Component(state).itemComponent();
+    document.querySelector(".stateComponent").innerHTML = new Component(state).statusComponent();
+    document.querySelector(".sp_stateComponent").innerHTML = new Component(state).statusComponent();
     document.querySelector(".materialComponent").innerHTML = materialComponent(materials);
     document.querySelector(".widthComponent").innerHTML = widthComponent(widths);
     document.querySelector(".caveComponent").innerHTML = caveComponent(caves);
